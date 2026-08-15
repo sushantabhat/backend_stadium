d84ee2346e3767644a4a18abb1aaa7eb7fb081af
Kcraman <roshnishrestha809@gmail.com>
2026-08-15 09:08:15 +0545

    server.js

--- server.js
+++ server.js
@@ -32,6 +32,10 @@ const mongoUri = envResult.parsed?.MONGO_URI || process.env.MONGO_URI;
 
 // Middleware
 app.use(cors());
+app.use((req, res, next) => {
+  console.log(`[${new Date().toLocaleTimeString()}] ${req.method} ${req.url}`);
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
